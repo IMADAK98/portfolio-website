17c29304f21a1735032818bb04ce345a9f38d1e2
Ali Elsayed <ali-hisham@hotmail.com>
2024-03-17 18:51:09 +0300

    fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -33,22 +33,18 @@ export default function RootLayout({
         className={`${inter.className}
       bg-gray-50 text-gray-950 relative pt-28 sm:pt-36
       dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90
-      `}
-      >
-        {/* <div
-          className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem]
-         h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]
-         dark:bg-[#946263]
-        "
-        ></div>
+      `}>
+        <div
+          className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 lg:right-1/4
+          aspect-square	 lg:w-[20%] rounded-full blur-[10rem]  
+          sm:w-[40%]           md:right-0         dark:bg-[#946263]
+        "></div>
 
         <div
-          className="bg-[#dbd7fb] absolute top-[-1 rem] -z-10 left-[-35rem]
-       h-[31.25rem] w-[50 rem] rounded-full blur-[10rem] sm:w-[68.75rem]
-       md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
-        dark:bg-[#676394]
-        "
-        ></div> */}
+          className="bg-[#dbd7fb] absolute top-[12rem] -z-10 lg:left-[10%]
+          aspect-square	lg:w-[30%] rounded-full blur-[10rem] dark:bg-[#676394]
+          sm:w-[40%]           sm:left-0
+        "></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
